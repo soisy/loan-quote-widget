527b7e00b9e6bbf276bcf66de4d6c60dc5c67adc
Kevin Cittadini <cittadini.kevin@gmail.com>
2020-10-14 10:24:33 +0200

    WIP

--- soisy/apps/loan-quote-widget/src/main.tsx
+++ soisy/apps/loan-quote-widget/src/main.tsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 
 import App from './app/app';
 
-var widgets = document.getElementsByClassName('soisy-loan-quote-widget');
-for (let i = 0; i < widgets.length; i++) {
+const soisyLoanQuotesWidgets = document.getElementsByClassName('soisy-loan-quote-widget');
+
+for (let i = 0; i < soisyLoanQuotesWidgets.length; i++) {
   ReactDOM.render(
     <React.StrictMode>
       <App />
     </React.StrictMode>,
-    widgets[i]   
+    soisyLoanQuotesWidgets[i]   
   );
 }
