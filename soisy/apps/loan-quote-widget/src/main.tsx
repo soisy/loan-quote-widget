29e68d1ec712fb3306f3afa46e3013b8c7c2dfde
Kevin Cittadini <cittadini.kevin@gmail.com>
2020-12-01 23:36:46 +0100

    Let it render new added widgets for 5s after dom has loaded.

--- soisy/apps/loan-quote-widget/src/main.tsx
+++ soisy/apps/loan-quote-widget/src/main.tsx
@@ -3,25 +3,44 @@ import ReactDOM from 'react-dom';
 
 import SoisyLoanQuoteWidget from './app/widget';
 
-const soisyLoanQuotesWidgets = document.getElementsByTagName('soisy-loan-quote');
+soisyWidgetInit();
 
-for (let i = 0; i < soisyLoanQuotesWidgets.length; i++) {
-    const widgetConfig = {
-        shopId: soisyLoanQuotesWidgets[i].attributes['shop-id'] ? soisyLoanQuotesWidgets[i].attributes['shop-id'].value : null,
-        amount: soisyLoanQuotesWidgets[i].attributes['amount'] ? soisyLoanQuotesWidgets[i].attributes['amount'].value : null,
-        instalments: soisyLoanQuotesWidgets[i].attributes['instalments'] ? soisyLoanQuotesWidgets[i].attributes['instalments'].value : null,
-        zeroInterestRate: soisyLoanQuotesWidgets[i].attributes['zero-interest-rate'] ? soisyLoanQuotesWidgets[i].attributes['zero-interest-rate'].value : null,
-    };
+function soisyWidgetInit() {
+    let currentIteration = 0;
+    const maxIterations = 5;
+    const tick = 1000;
 
-    ReactDOM.render(
-        <React.StrictMode>
-            <SoisyLoanQuoteWidget
-                shopId={widgetConfig.shopId}
-                amount={widgetConfig.amount}
-                instalments={widgetConfig.instalments}
-                zeroInterestRate={widgetConfig.zeroInterestRate}
-            />
-        </React.StrictMode>,
-        soisyLoanQuotesWidgets[i]
-    );
+    const soisyInterval = setInterval(() => {
+        if (currentIteration >= maxIterations) {
+            clearInterval(soisyInterval);
+            return;
+        }
+
+        renderSoisyWidgets();
+        currentIteration++;
+    }, tick);
+}
+
+function renderSoisyWidgets() {
+    const soisyLoanQuotesWidgets = document.getElementsByTagName('soisy-loan-quote');
+    for (let i = 0; i < soisyLoanQuotesWidgets.length; i++) {
+        const widgetConfig = {
+            shopId: soisyLoanQuotesWidgets[i].attributes['shop-id'] ? soisyLoanQuotesWidgets[i].attributes['shop-id'].value : null,
+            amount: soisyLoanQuotesWidgets[i].attributes['amount'] ? soisyLoanQuotesWidgets[i].attributes['amount'].value : null,
+            instalments: soisyLoanQuotesWidgets[i].attributes['instalments'] ? soisyLoanQuotesWidgets[i].attributes['instalments'].value : null,
+            zeroInterestRate: soisyLoanQuotesWidgets[i].attributes['zero-interest-rate'] ? soisyLoanQuotesWidgets[i].attributes['zero-interest-rate'].value : null,
+        };
+
+        ReactDOM.render(
+            <React.StrictMode>
+                <SoisyLoanQuoteWidget
+                    shopId={widgetConfig.shopId}
+                    amount={widgetConfig.amount}
+                    instalments={widgetConfig.instalments}
+                    zeroInterestRate={widgetConfig.zeroInterestRate}
+                />
+            </React.StrictMode>,
+            soisyLoanQuotesWidgets[i]
+        );
+    }
 }
