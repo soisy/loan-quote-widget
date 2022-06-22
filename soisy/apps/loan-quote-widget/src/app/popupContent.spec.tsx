f27932732fd3a5e1e5c29d588fd8aa876db1e3a3
salvatore-disavio <salvatore.disavio99@gmail.com>
2022-06-22 11:49:59 +0200

    Fix build
    Add "Esempio da"

--- soisy/apps/loan-quote-widget/src/app/popupContent.spec.tsx
+++ soisy/apps/loan-quote-widget/src/app/popupContent.spec.tsx
@@ -28,7 +28,7 @@ describe('Popup content', () => {
                 max={max}
             />
         );
-        expect(content.text()).toMatch(/Da 66,10 € per 12 mesi,/);
+        expect(content.text()).toMatch(/Esempio da 66,10 € per 12 mesi,/);
         expect(content.text()).toMatch(/TAN da 7,50% a 11,50% - TAEG da 5,50% a 14,50%/);
         expect(content.text()).toMatch(/Spesa complessiva da 1.350,00€ a 1.400,00€/);
     });
@@ -56,7 +56,7 @@ describe('Popup content', () => {
                 max={max}
             />
         );
-        expect(content.text()).toMatch(/Da 66,10 € per 12 mesi a tasso zero,/);
+        expect(content.text()).toMatch(/Esempio da 66,10 € per 12 mesi a tasso zero,/);
         expect(content.text()).toMatch(/TAEG 0,00%/);
         expect(content.text()).toMatch(/Spesa complessiva 1.400,00€/);
     });
