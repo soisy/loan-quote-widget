f36f17af4328b5aad7c8b9bef2a7d0401890d8c9
salvatore-disavio <salvatore.disavio99@gmail.com>
2022-06-22 11:32:09 +0200

    Add "Esempio"

--- soisy/apps/loan-quote-widget/src/app/sentence.tsx
+++ soisy/apps/loan-quote-widget/src/app/sentence.tsx
@@ -3,7 +3,7 @@ import {Text, Price} from '../assets/styled-components/sentence';
 
 const QuoteSentence = (props) => {
     return (
-        <Text>da <Price>{props.amount} €</Price> per <b>{props.instalments} mesi</b>{props.zeroInterestRate ? (<b> a tasso zero</b>) : ''} con</Text>
+        <Text>Esempio da <Price>{props.amount} €</Price> per <b>{props.instalments} mesi</b>{props.zeroInterestRate ? (<b> a tasso zero</b>) : ''} con</Text>
     );
 }
 
